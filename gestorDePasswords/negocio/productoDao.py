fca4a16a1e42d408684e8564b7153f4046e57622
MateoValenzuela <mateo.valenzuela.dev@gmail.com>
2022-10-17 21:02:14 -0300

    Actualización diaria

--- gestorDePasswords/negocio/productoDao.py
+++ gestorDePasswords/negocio/productoDao.py
@@ -4,11 +4,30 @@ from negocio.producto import ClsProductos
 class ClsProductosDao:
 
     @classmethod
-    def buscarIndiceProducto(cls, producto):
-        varCont = 0
-        varListaProductos = ClsProductos.listaObjetosProductos
-        for i in varListaProductos:
-            if i.producto == producto:
-                return varCont
-            else:
-                varCont += 1
+    def insertarProducto(cls, objProducto):
+        varProducto = objProducto.producto
+        varCantidad = objProducto.cantidad
+        varDescripcion = objProducto.descripcion
+        varPrecioUnitario = objProducto.precioUnitario
+        varimporte = objProducto.importe
+
+        INSERT = cls.INSERTAR + f'''(\'{varNombre}\', \'{varApellido}\', \'{varApodo}\', \'{varSexo}\', \'{varNacionalidad}\',
+    \'{varFechaNacimiento}\', {varEsFeliz}, {varEdad}, {varDni}, {varAltura})'''
+        ClsConexion.ejecutarSql(INSERT)
+
+
+
+
+    @classmethod
+    def obtenerProductosPorListaDeId(cls, listaDeId):
+        listaDeProductos  = []
+        for i in listaDeId:
+            if i == '-':
+                continue
+            selectProducto = f'sentencia sql con id{i}'
+            #ejecutar sentencia
+            productoObtenido = 'pepe'
+
+            for registro in productoObtenido:
+                producto = ClsProductos(registro[0], registro[1], registro[2], registro[3], registro[4], registro[5])
+                listaDeProductos.append(producto)
